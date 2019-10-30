ba966e6d733d04d82c447251387ac65e1af21378
pranit <tarepranit505@gmail.com>
2019-10-30 12:04:09 +0530

    revert

---
+++ demonew/src/app/shopping-cart/shopping.model.ts
@@ -0,0 +1,7 @@
+export class Shopping{
+    constructor(public name: string, 
+        public amount: number,
+         public description: string,
+         public imageurl?: string
+         ) {}
+}
