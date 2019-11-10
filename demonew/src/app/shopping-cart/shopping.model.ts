64bbd6485dc1e10a83849a82cfda07a46351bea8
pranit <tarepranit505@gmail.com>
2019-11-10 12:25:52 +0530

    reset

--- demonew/src/app/shopping-cart/shopping.model.ts
+++
@@ -1,9 +0,0 @@
-export class Shopping{
-    constructor(
-        public id: number,
-        public name: string, 
-        public amount: number,
-         public description: string,
-         public imageurl?: string
-         ) {}
-}
