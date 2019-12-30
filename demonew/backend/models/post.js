48f66f05c75297cf86550bbc1c2131441ba9b67b
pranit <tarepranit505@gmail.com>
2019-12-30 17:58:03 +0530

    reset

--- demonew/backend/models/post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-    content: { type: String, required: true }
-});
-
-
-module.exports = mongoose.model('Post', postSchema);
