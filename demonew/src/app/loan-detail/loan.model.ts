ee59843f9e96f8b06ab4de9994af6f3975678491
pranit <tarepranit505@gmail.com>
2020-01-02 15:58:19 +0530

    reset

--- demonew/src/app/loan-detail/loan.model.ts
+++
@@ -1,6 +0,0 @@
-export class Loan {
-    constructor(public loanAmount: any, public existingEmi: any) {
-        this.loanAmount = loanAmount;
-        this.existingEmi = existingEmi;
-    }
-}
--- demonew/src/app/personal-detail/personal.model.ts
+++
@@ -1,9 +0,0 @@
-export class Personal {
-    constructor(public date: Date, public email: string, public first:string, public last: string, public mobile: number) {
-        this.date = date;
-        this.email = email;
-        this.first = first;
-        this.last = last;
-        this.mobile = mobile;
-    }
-}
